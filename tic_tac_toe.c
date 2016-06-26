8a6a93cc0af653a91f278dbebce74c1d6fb9edda
unknown <akhilcool1996@gmail.com>
2016-06-26 13:32:35 +0530

    added screen clear after every turn

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,13 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
 #define MAX_DIMENSION 3
+
 
 void mark(int markRow,int markColumn,char c);
 void display();
 int check();
 
+
 //global variables
 char arr[3][3];																	//the tic-tac-toe matrix
 int flag[3][3];																	//for checking if a given position is occupied or not
+char player1Name[10],player2Name[10];
 
 
 int main()
@@ -16,12 +20,25 @@ int main()
 	for(i=0;i<MAX_DIMENSION;i++)
 		for(j=0;j<MAX_DIMENSION;j++)
 			arr[i][j]=' ';
+
+	printf("Player 1 Enter your name: ");
+	scanf("%s",player1Name);
+	printf("Player 2 Enter your name: ");
+	scanf("%s",player2Name);
+	system("cls");
+	display();
+
 	while(moveCount<9&&WIN_status==0)
 	{
+		if(turn==1)
+			printf("\n%s your turn.",player1Name);
+		else
+			printf("\n%s your turn.",player2Name);
 		printf("\nEnter the Row:");
 		scanf("%d",&markRow);
 		printf("Enter the Column:");
 		scanf("%d",&markColumn);
+		system("cls");
 		if(turn==1)
 		{
 			mark(markRow,markColumn,'X');
@@ -38,12 +55,14 @@ int main()
 	}
 }
 
+
 //----------------------------------start of mark function.--------------------------------------------
 //marks the player symbol on given position
 void mark(int markRow,int markColumn,char c)
 {
 	arr[markRow-1][markColumn-1]=c;
 }
+
 
 //----------------------------------start of display function.--------------------------------------------
 //displays the current status of tic-tac-toe matrix
@@ -62,6 +81,7 @@ void display()
 		printf("\n-----------\n");
 	}
 }
+
 
 //----------------------------------start of check function.--------------------------------------------
 //checks for win condition
